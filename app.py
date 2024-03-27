3b4442503c460534b62a6196bad8cbf40222074f
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-03-28 03:22:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,50 +1,18 @@
-import torch
-from diffusers import (
-    StableDiffusionXLPipeline, 
-    EulerAncestralDiscreteScheduler,
-    AutoencoderKL
-)
-from io import BytesIO
-import base64
-import os
-
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
 class InferlessPythonModel:
     def initialize(self):
-        # Load VAE component
-        self.vae = AutoencoderKL.from_pretrained(
-            "madebyollin/sdxl-vae-fp16-fix", 
-            torch_dtype=torch.float16
-        )
-        
-        # Configure the pipeline
-        self.pipe = StableDiffusionXLPipeline.from_pretrained(
-            "cagliostrolab/animagine-xl-3.0", 
-            vae=self.vae,
-            torch_dtype=torch.float16, 
-            use_safetensors=True, 
-        )
-        self.pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(self.pipe.scheduler.config)
-        self.pipe.to('cuda')
+        model_id = "CohereForAI/c4ai-command-r-v01"
+        self.tokenizer = AutoTokenizer.from_pretrained(model_id)
+        self.model = AutoModelForCausalLM.from_pretrained(model_id)
 
-
-    def infer(self, inputs):
+    def infer(self,inputs):
         prompt = inputs["prompt"]
-        negative_prompt = inputs["negative_prompt"]
-        
-        image = self.pipe(
-            prompt, 
-            negative_prompt=negative_prompt, 
-            width=832,
-            height=1216,
-            guidance_scale=7,
-            num_inference_steps=28
-        ).images[0]
-
-        buff = BytesIO()
-        image.save(buff, format="JPEG")
-        img_str = base64.b64encode(buff.getvalue()).decode()
-        return { "generated_image_base64" : img_str }
+        messages = [{"role": "user", "content": prompt}]
+        input_ids = self.tokenizer.apply_chat_template(messages, tokenize=True, add_generation_prompt=True, return_tensors="pt")
+        generated_tokens = self.model.generate(input_ids,max_new_tokens=256,do_sample=True,temperature=0.1)
+        generated_text = self.tokenizer.batch_decode(generated_tokens[:, input_ids.shape[1]:], skip_special_tokens=True)[0]
+        return { "generated_text" : generated_text}
         
     def finalize(self):
         self.pipe = None
