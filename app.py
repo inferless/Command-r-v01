4a2c42b6c519e491557df41587c1227fd01b3626
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-03-31 19:30:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM , BitsAndBytesConfig
 
 class InferlessPythonModel:
     def initialize(self):
         model_id = "CohereForAI/c4ai-command-r-v01"
         self.tokenizer = AutoTokenizer.from_pretrained(model_id)
-        self.model = AutoModelForCausalLM.from_pretrained(model_id)
+        bnb_config = BitsAndBytesConfig(load_in_8bit=True)
+        self.model = AutoModelForCausalLM.from_pretrained(model_id,quantization_config=bnb_config,device_map = 'cuda')
 
     def infer(self,inputs):
         prompt = inputs["prompt"]
@@ -13,6 +14,6 @@ class InferlessPythonModel:
         generated_tokens = self.model.generate(input_ids,max_new_tokens=256,do_sample=True,temperature=0.1)
         generated_text = self.tokenizer.batch_decode(generated_tokens[:, input_ids.shape[1]:], skip_special_tokens=True)[0]
         return { "generated_text" : generated_text}
-        
+
     def finalize(self):
         self.pipe = None
