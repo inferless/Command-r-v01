9129b191dba01d8516f1980c1f8a921712d94d03
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2025-04-16 09:12:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ class InferlessPythonModel:
     def infer(self,inputs):
         prompt = inputs["prompt"]
         messages = [{"role": "user", "content": prompt}]
-        input_ids = self.tokenizer.apply_chat_template(messages, tokenize=True, add_generation_prompt=True, return_tensors="pt")
+        input_ids = self.tokenizer.apply_chat_template(messages, tokenize=True, add_generation_prompt=True, return_tensors="pt").to("cuda")
         generated_tokens = self.model.generate(input_ids,max_new_tokens=256,do_sample=True,temperature=0.1)
         generated_text = self.tokenizer.batch_decode(generated_tokens[:, input_ids.shape[1]:], skip_special_tokens=True)[0]
         return { "generated_text" : generated_text}
